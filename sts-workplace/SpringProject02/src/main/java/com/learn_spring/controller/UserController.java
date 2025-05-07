9da94f00ca0bbc57685be635c9873836c3d57010
srohiit2678 <rohitsikhwal99@gmail.com>
2025-05-07 16:45:42 +0530

    Day-2 Spring Work

--- sts-workplace/SpringProject02/src/main/java/com/learn_spring/controller/UserController.java
+++ sts-workplace/SpringProject02/src/main/java/com/learn_spring/controller/UserController.java
@@ -57,6 +57,40 @@ public class UserController {
 		return li.toString();
 	}
 	
+	@GetMapping("/filters/{id}/categori/{name}")
+	public String getItemByPath(@PathVariable String id,@PathVariable String name)
+	{
+		List<String> li = new ArrayList<>();
+		li.add("1-Item"); li.add("2-Item"); li.add("3-Item"); li.add("4-Item");
+		li.add("5-Item"); li.add("6-Item"); li.add("7-Item"); li.add("8-Item");
+		li.add("9-Item"); li.add("10-Item");
+		
+		List<String> service = new ArrayList<>();
+		
+		service.add("Frutes"); service.add("vegitables"); service.add("Protine");
+		service.add("Electorics"); service.add("Cloths"); service.add("Beauty Products");
+		service.add("Sports Hub"); service.add("Toys"); service.add("Home Services");
+		service.add("Travlel");
+		
+		String  item = "";
+		if(id != null)
+		{
+			for(String s1 : li)
+			{
+				if(s1.contains(id))
+					item = s1;
+			}
+			for(String s2 : service)
+			{
+				if(s2.contains(name))
+					return item +" --> "+ s2;
+			}
+			
+			return "No Match Found";
+		}
+		return li.toString();
+	}
+	
 	@PostMapping("/filters")
 	public String getItem(@RequestParam(required = false)String id)
 	{
